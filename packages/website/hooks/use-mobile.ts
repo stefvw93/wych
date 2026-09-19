842dcca7d94aad53f9fd96719765eab61f3dad79
stefvw93 <stef@stefvanwijchen.com>
2026-09-19 09:48:15 +0200

    add client/window check

--- packages/website/hooks/use-mobile.ts
+++ packages/website/hooks/use-mobile.ts
@@ -4,7 +4,7 @@ const MOBILE_BREAKPOINT = 768;
 
 export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(() => {
-    return window.innerWidth < MOBILE_BREAKPOINT;
+    return "window" in globalThis ? window.innerWidth < MOBILE_BREAKPOINT : false;
   });
 
   React.useEffect(() => {
